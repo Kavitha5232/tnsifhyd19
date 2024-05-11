16b68ebb5367fee1a9c51313fb80a67d6ee41e36
Kavitha5232 <168549797+Kavitha5232@users.noreply.github.com>
2024-05-11 11:35:55 +0530

    Approach1 file uploaded

---
+++ Approach1.java
@@ -0,0 +1,23 @@
+package com.kavitha.sample;
+
+public class Approach1 {
+	 String batsman="virat kohili";
+	static String bowler="bhuvi";
+	void display() {
+		System.out.println("cricket");
+	}
+	static String display1() {
+		return "Teams";
+	}
+	public static void main(String[] args)
+	{
+		Approach1 A1=new Approach1();
+		System.out.println(A1.batsman);
+		System.out.println(Approach1.bowler);
+		A1.display();
+		Approach1.display1();
+	}
+}
+		
+		
+	
